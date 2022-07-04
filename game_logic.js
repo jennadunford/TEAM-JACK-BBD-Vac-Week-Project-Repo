f47905d6ec9f33f66cf55939eea11aafca5acebb
tomSteve25 <tds251000@gmail.com>
2022-07-04 12:15:09 +0200

    Added game flow skeleton

--- game_logic.js
+++ game_logic.js
@@ -6,3 +6,28 @@
 // currentRound
 // timeRemaining
 
+//server would have supplied users at this point. Will be a list of objects:
+// var player = {
+//     id = ,
+//     score = 0,
+//     playing
+// }
+var playerList = {};
+
+var maxRounds = 3;
+
+var gamestate = {
+    leaderboard,
+    currentSong,
+    sensitivity,
+    currentRound,
+    timeRemaining
+};
+
+function sortLeaderboard(playerList){
+    //sorts players according to score
+}
+
+function playGame(){
+
+}
