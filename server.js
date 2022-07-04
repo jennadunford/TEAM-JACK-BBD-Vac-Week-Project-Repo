9708dc22f29ec6349dfecbee9bbf7f02326c1f80
DvZ02 <danielvzyl1469@gmail.com>
2022-07-04 15:10:44 +0200

    Server Update
    Added:
    Function to get players
    Generating game code

--- server.js
+++ server.js
@@ -7,8 +7,33 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+let code = genCode(4);
+
+let players = [];
+let playerCount = 0;
+
 io.on('connection', (socket) => { //Evertything with socket
     console.log('Client connected');
+
+    socket.on('User', (user) => {
+        players[playerCount++] = {
+            id: user,
+            playing: true,
+            score: 0,
+            acceleration: 0
+        }
+
+        console.log(players);
+    })
+
+    socket.on('joinCode', (clientCode) => {
+        if(clientCode !== code){
+            socket.disconnect(true);
+            console.log("Connection closed");
+        }
+    });
+
+    socket.broadcast.emit('gameCode', code);
 });
 
 
@@ -16,4 +41,21 @@ app.use(express.static(path.join(__dirname, 'Client')));
 
 server.listen(9000, () => { //Port server listen on
     console.log("Listening on 9000");
+    console.log(code);
 });
+
+function genCode(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+   return result;
+}
+
+function getPlayers(){
+    return players;
+}
+
+module.exports = { getPlayers };
