cd9e720c6c39a7b3d7b4443f8300c034b8dca0a4
Daviddo071 <david.budnitsky.2001@gmail.com>
2022-07-04 12:57:36 +0200

    Comments
    Comments addded for gamestate var
    changed getSensitivity to getAcceleration
    Comments in general

--- game_logic.js
+++ game_logic.js
@@ -10,12 +10,23 @@
 // var player = {
 //     id = ,
 //     score = 0,
-//     playing
+//     playing = True,
+//     acceleration = 0
 // }
+
+// playerList consists of all players in the game. a list/array of player variables
 var playerList = {};
 
+//hardcode for now, change once MVP is met
 var maxRounds = 3;
 
+// state of current game. attributes described below:
+    // leaderboard: sorted playerList. name is schanged for gamestate object. sorted in descending order
+    // currentSong: path to which current song is. file path is:
+    // sensitivity: sensitivity as determined from the tempo etc of the song being played rn
+    // currentRound: current round, best of 3 atm
+    // timeRemaining: time remaining of current round. as time goes on, sensitivity sensitivity threshold increases
+//  
 var gamestate = {
     leaderboard,
     currentSong,
@@ -26,12 +37,13 @@ var gamestate = {
 
 function sortLeaderboard(playerList){
     //sorts players according to score
+
 }
 
-function getSensitity(accReading){
-    
+function getAcceleration(accReading){
+    // gets the acceleration of each player from the accelerometer
 }
 
 function playGame(){
-
+    // game play logic
 }
