f988dff59c28d9da9bf5538ff1e8ebeefe6cb40c
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-07-05 16:19:40 +0200

    Changing host 2 location

--- Interface/js/main.js
+++ Interface/js/main.js
@@ -4,12 +4,8 @@ const socket = new io("http://localhost:9000", {});
 
 function makeHost() {
   console.log("MAKE HOST");
-<<<<<<< Updated upstream
-  window.location.href = "hostv2.html";
-=======
   window.location.href = "./hostv2.html";
 
->>>>>>> Stashed changes
   // socket.emit("User", document.getElementById("userName").value);
   // socket.emit("joinCode", document.getElementById("joinCode").value);
 }
