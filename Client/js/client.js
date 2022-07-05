dfbd3b69c324e1b87e68c705a75ef9f37c7d1836
SwagmasterKDizzle <keeganoreilly8@gmail.com>
2022-07-05 12:12:39 +0200

    Update client.js

--- Client/js/client.js
+++ Client/js/client.js
@@ -22,7 +22,7 @@ acl.addEventListener('reading', () => {
   console.log("Acceleration along the Y-axis " + acl.y);
   console.log("Acceleration along the Z-axis " + acl.z);
 
-  alert("Acceleration along the X-axis " + acl.x);
+  alert("Acceleration along the X-axis " + acl.x + ", Y-axis: " + acl.y + ", Z-axis: " + acl.z);
 });
 
 acl.start();
