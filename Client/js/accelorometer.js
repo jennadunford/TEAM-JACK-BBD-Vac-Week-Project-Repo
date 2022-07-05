10afd54dd28a20603fcf137d53ead33f1cb7d4c1
DvZ02 <danielvzyl1469@gmail.com>
2022-07-05 10:58:56 +0200

    OpenSSL testing
    Nothing working

---
+++ Client/js/accelorometer.js
@@ -0,0 +1,10 @@
+let acl = new Accelerometer({frequency: 60});
+acl.addEventListener('reading', () => {
+  console.log("Acceleration along the X-axis " + acl.x);
+  console.log("Acceleration along the Y-axis " + acl.y);
+  console.log("Acceleration along the Z-axis " + acl.z);
+
+  alert("Acceleration along the X-axis " + acl.x);
+});
+
+acl.start();
--- server.js
+++ server.js
@@ -7,18 +7,19 @@ const socketio = require('socket.io');
 const port = process.env.PORT || 9000;
 
 //Open SSL stuff
-const openssl = require('node-openssl-cert');
+// const openssl = require('node-openssl-cert');
 
-var options = {
-	binpath: 'D:/BBD/Vac Weeks/2022/OpenSSL-Win64/bin/openssl.exe'
-}
+// var options = {
+// 	binpath: 'D:/BBD/Vac Weeks/2022/OpenSSL-Win64/bin/openssl.exe'
+// }
 
-const open = new openssl(options);
+// const open = new openssl(options);
 
-open.generateRSAPrivateKey({}, function(err, key, cmd) {
-	console.log(cmd);
-	console.log(key);
-});
+// open.generateRSAPrivateKey({}, function(err, key, cmd) {
+// 	console.log(cmd);
+// 	console.log(key);
+// });
+//
 
 const app = express();
 const server = http.createServer(app);
