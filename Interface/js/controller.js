b9f0f173aacd299d956037e2702559e4d6971f51
Daviddo071 <david.budnitsky.2001@gmail.com>
2022-07-05 16:48:41 +0200

    Updated controller.js to linear not acc

--- Interface/js/controller.js
+++ Interface/js/controller.js
@@ -11,6 +11,7 @@ var output;
 var xOutput = document.getElementById("xRead");
 var yOutput = document.getElementById("yRead");
 var zOutput = document.getElementById("zRead");
+var normOutput = document.getElementById("norm")
 
 
 $("#readyButton").click(function () {
@@ -76,12 +77,14 @@ socket.on('validCode', () => {
 });
 
 function updateReadings() {
-  let acl = new Accelerometer({ frequency: 60 });
+  let acl = new LinearAccelerationSensor({ frequency: 60 });
   acl.addEventListener("reading", () => {
     //console.log("Acceleration along the X-axis " + acl.x);
     xOutput.innerHTML = acl.x.toFixed(2);
     yOutput.innerHTML = acl.y.toFixed(2);
     zOutput.innerHTML = acl.z.toFixed(2);
+
+    normOutput.innerHTML = Math.sqrt((acl.x*acl.x) + (acl.y*acl.y) + (acl.z*acl.z)).toFixed(2)
 
     // console.log("Acceleration along the Y-axis " + acl.y);
     // console.log("Acceleration along the Z-axis " + acl.z);
