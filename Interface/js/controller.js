515de515ccaee5c0d8fd4678259329e2b31f7295
SwagmasterKDizzle <keeganoreilly8@gmail.com>
2022-07-05 13:47:55 +0200

    Update controller.js

--- Interface/js/controller.js
+++ Interface/js/controller.js
@@ -30,3 +30,14 @@ $("#readyButton").click(function () {
     }
   }
 });
+
+let acl = new Accelerometer({frequency: 60});
+acl.addEventListener('reading', () => {
+  console.log("Acceleration along the X-axis " + acl.x);
+  console.log("Acceleration along the Y-axis " + acl.y);
+  console.log("Acceleration along the Z-axis " + acl.z);
+
+  alert("Acceleration along the X-axis " + acl.x + ", Y-axis: " + acl.y + ", Z-axis: " + acl.z);
+});
+
+acl.start();
