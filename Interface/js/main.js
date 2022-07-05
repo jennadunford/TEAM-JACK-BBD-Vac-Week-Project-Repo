7bed3f8e9dc71f8bc5393cf12382cb0318872610
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-07-05 14:59:08 +0200

    Making Host V2

--- Interface/js/main.js
+++ Interface/js/main.js
@@ -1,16 +1,15 @@
 // lets go
-console.log("MAIN JS")
+console.log("MAIN JS");
 const socket = new io("http://localhost:9000", {});
 
-function makeHost()
-{
-    console.log('MAKE HOST');
-    window.location.href = './html/host.html'
-    // socket.emit("User", document.getElementById("userName").value);
-    // socket.emit("joinCode", document.getElementById("joinCode").value);
+function makeHost() {
+  console.log("MAKE HOST");
+  window.location.href = "hostv2.html";
+  // socket.emit("User", document.getElementById("userName").value);
+  // socket.emit("joinCode", document.getElementById("joinCode").value);
 }
-function makeController(){
-    console.log('MAKE CONTROLLER');
-    window.location.href = './html/controller.html'
-    // socket.emit("ready");
+function makeController() {
+  console.log("MAKE CONTROLLER");
+  window.location.href = "controller.html";
+  // socket.emit("ready");
 }
