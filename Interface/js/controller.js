fa5894bd240872ca92cdf022576ea1d358d984f5
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-07-05 15:09:31 +0200

    Added readings

--- Interface/js/controller.js
+++ Interface/js/controller.js
@@ -6,6 +6,9 @@ var uName = document.getElementById("uName");
 var jCode = document.getElementById("jCode");
 var userReady = false;
 var output;
+var xOutput = document.getElementById("xRead");
+var yOutput = document.getElementById("yRead");
+var zOutput = document.getElementById("zRead");
 
 $("#readyButton").click(function () {
   if (userName.value == "" || joinCode.value == "") {
@@ -31,23 +34,47 @@ $("#readyButton").click(function () {
   }
 });
 
-function alertFunc() {
+// function alertFunc() {
+//   let acl = new Accelerometer({ frequency: 60 });
+//   acl.addEventListener("reading", () => {
+//     console.log("Acceleration along the X-axis " + acl.x);
+//     console.log("Acceleration along the Y-axis " + acl.y);
+//     console.log("Acceleration along the Z-axis " + acl.z);
+
+//     alert(
+//       "Acceleration along the X-axis " +
+//         acl.x +
+//         ", Y-axis: " +
+//         acl.y +
+//         ", Z-axis: " +
+//         acl.z
+//     );
+//   });
+//   acl.start();
+// }
+
+//setInterval(alertFunc(), 10000); //for some reason, still constant, unstoppable updates...
+
+function updateReadings() {
   let acl = new Accelerometer({ frequency: 60 });
   acl.addEventListener("reading", () => {
-    console.log("Acceleration along the X-axis " + acl.x);
-    console.log("Acceleration along the Y-axis " + acl.y);
-    console.log("Acceleration along the Z-axis " + acl.z);
+    //console.log("Acceleration along the X-axis " + acl.x);
+    xOutput.innerHTML = acl.x;
+    yOutput.innerHTML = acl.y;
+    zOutput.innerHTML = acl.z;
 
-    alert(
-      "Acceleration along the X-axis " +
-        acl.x +
-        ", Y-axis: " +
-        acl.y +
-        ", Z-axis: " +
-        acl.z
-    );
+    // console.log("Acceleration along the Y-axis " + acl.y);
+    // console.log("Acceleration along the Z-axis " + acl.z);
+
+    // alert(
+    //   "Acceleration along the X-axis " +
+    //     acl.x +
+    //     ", Y-axis: " +
+    //     acl.y +
+    //     ", Z-axis: " +
+    //     acl.z
+    // );
   });
   acl.start();
 }
-
-setInterval(alertFunc(), 10000); //for some reason, still constant, unstoppable updates...
+setInterval(updateReadings(), 100);
