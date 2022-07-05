17b58370a908e79ba4f447ab81e8088723d1464b
SwagmasterKDizzle <keeganoreilly8@gmail.com>
2022-07-05 14:35:07 +0200

    Update controller.js

--- Interface/js/controller.js
+++ Interface/js/controller.js
@@ -36,8 +36,9 @@ $("#readyButton").click(function () {
 
 function alertFunc()
 {
+  let acl = new Accelerometer({frequency: 60});
   acl.addEventListener('reading', () => {
-    let acl = new Accelerometer({frequency: 60});
+    
     console.log("Acceleration along the X-axis " + acl.x);
     console.log("Acceleration along the Y-axis " + acl.y);
     console.log("Acceleration along the Z-axis " + acl.z);
