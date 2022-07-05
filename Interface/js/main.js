af6dffa8e435dc6d0d80aa7ff7be877698b36b7f
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-07-05 13:44:41 +0200

    fixed referencing of js and css and html

--- Interface/js/main.js
+++ Interface/js/main.js
@@ -1,16 +1,15 @@
 // lets go
-console.log("MAIN JS")
+console.log("MAIN JS");
 const socket = new io("https://damp-gorge-23211.herokuapp.com/", {});
 
-function makeHost()
-{
-    console.log('MAKE HOST');
-    window.location.href = './html/host.html'
-    // socket.emit("User", document.getElementById("userName").value);
-    // socket.emit("joinCode", document.getElementById("joinCode").value);
+function makeHost() {
+  console.log("MAKE HOST");
+  window.location.href = "./host.html";
+  // socket.emit("User", document.getElementById("userName").value);
+  // socket.emit("joinCode", document.getElementById("joinCode").value);
 }
-function makeController(){
-    console.log('MAKE CONTROLLER');
-    window.location.href = './html/controller.html'
-    // socket.emit("ready");
+function makeController() {
+  console.log("MAKE CONTROLLER");
+  window.location.href = "./controller.html";
+  // socket.emit("ready");
 }
