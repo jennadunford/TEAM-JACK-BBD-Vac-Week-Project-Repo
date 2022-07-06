510a0fa52a7308adaf19de9cbfd97859725d68e9
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-07-06 11:41:30 +0200

    Added player screen and strikethrough

---
+++ Interface/js/addRemove.js
@@ -0,0 +1,30 @@
+function addPlayer(userName) {
+  const node = document.createElement("li");
+  const textnode = document.createTextNode(userName);
+  node.appendChild(textnode);
+  document.getElementById("playerList").appendChild(node);
+}
+
+function removePlayer(userName) {
+  let nodes = Array.from($("#playerList").children("li"));
+  for (let count = 0; count < nodes.length; count++) {
+    const element = nodes[count];
+    console.log(nodes[count].innerHTML);
+    if (element.innerHTML == userName) {
+      element.parentNode.removeChild(element);
+      break;
+    }
+  }
+}
+
+function strikeThrough(userName) {
+  let nodes = Array.from($("#playerList").children("li"));
+  for (let count = 0; count < nodes.length; count++) {
+    const element = nodes[count];
+    console.log(nodes[count].innerHTML);
+    if (element.innerHTML == userName) {
+      element.innerHTML.strike();
+      break;
+    }
+  }
+}
