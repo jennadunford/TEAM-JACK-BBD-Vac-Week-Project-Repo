6be8d2cf81ecdaf8d43cdd49eb55bbe5581ef0e9
tomSteve25 <tds251000@gmail.com>
2022-07-06 18:19:24 +0200

    test

--- Interface/js/computerUI.js
+++ Interface/js/computerUI.js
@@ -1,4 +1,4 @@
-// var userName = document.querySelector("#userName");
+var userName = document.querySelector("#userName");
 // const socket = new io("http://localhost:9000", {});
 // console.log("computer ui");
 const socket = new io("https://damp-gorge-23211.herokuapp.com/", {});
