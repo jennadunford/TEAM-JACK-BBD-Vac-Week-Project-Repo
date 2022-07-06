e59b057822e30788e921f7a18ecaa7cd4c482343
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-07-06 08:36:58 +0200

    Added remove player function

--- Interface/js/computerUI.js
+++ Interface/js/computerUI.js
@@ -1,6 +1,9 @@
 // var userName = document.querySelector("#userName");
 // const socket = new io("http://localhost:9000", {});
 // console.log("computer ui");
+
+var userName = document.getElementById("userName");
+
 $("#generateButton").click(function () {
   var joinCodeDisplay = document.getElementById("joinCode");
   joinCodeDisplay.innerHTML = "ABCD";
@@ -25,3 +28,18 @@ $("#startButton").click(function () {
   $("#startGamePressed").fadeIn(500);
   $("#startGamePressed").fadeOut(500);
 });
+
+$("#removePlayer").click(function () {
+  removePlayer(userName.value);
+});
+function removePlayer(userName) {
+  let nodes = Array.from($("#playerList").children("li"));
+  for (let count = 0; count < nodes.length; count++) {
+    const element = nodes[count];
+    console.log(nodes[count].innerHTML);
+    if (element.innerHTML == userName) {
+      element.parentNode.removeChild(element);
+      break;
+    }
+  }
+}
