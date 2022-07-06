faa3a271115ca554a4368071f61c1f9fed826167
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-07-06 13:07:42 +0200

    updated div

--- Interface/js/controller.js
+++ Interface/js/controller.js
@@ -24,8 +24,8 @@ var lower_threshold = 0;
 var upper_threshold = 30;
 var hard_cap = 50;
 
-var updateState = window.getElementById("updateState");
-
+var updateState = document.getElementById("updateState");
+var updateMag = document.getElementById("updateMag");
 
 // $("#readyButton").click(function () {
 //   console.log("ready button pressed");
@@ -117,7 +117,6 @@ socket.on("invalidCode", () => {
 socket.on("validCode", () => {
   alert("Client: Code was accepted");
   console.log("Client: Code was accepted");
-
 });
 
 socket.on("updateSensitivity", (songSensitivity) => {
@@ -133,7 +132,6 @@ socket.on("updateSensitivity", (songSensitivity) => {
   }
 
   // TODO: check that song sensitivity makes sense for thresholds
-
 });
 
 function updateReadings() {
@@ -144,11 +142,11 @@ function updateReadings() {
     yOutput.innerHTML = acl.y.toFixed(2);
     zOutput.innerHTML = acl.z.toFixed(2);
 
-    sensorAccelerationMagnitude = Math.sqrt(acl.x * acl.x + acl.y * acl.y + acl.z * acl.z);
+    sensorAccelerationMagnitude = Math.sqrt(
+      acl.x * acl.x + acl.y * acl.y + acl.z * acl.z
+    );
 
     normOutput.innerHTML = sensorAccelerationMagnitude.toFixed(2);
-
-    
 
     // console.log("Acceleration along the Y-axis " + acl.y);
     // console.log("Acceleration along the Z-axis " + acl.z);
@@ -166,10 +164,8 @@ function updateReadings() {
   return sensorAccelerationMagnitude;
 }
 
-
-function alert_disqualify(acc_magnitude){ 
-  if (acc_magnitude>=upper_threshold || acc_magnitude > hard_cap){
-  
+function alert_disqualify(acc_magnitude) {
+  if (acc_magnitude >= upper_threshold || acc_magnitude > hard_cap) {
     // disqualify the player:
     alert("Disqualified");
     // tell player that player is disqualified by making their screen red
@@ -179,7 +175,7 @@ function alert_disqualify(acc_magnitude){
     // tell server that player is disqualifyed
     socket.emit("disqualifyPlayer", sessionStorage.getItem("userName"));
 
-  //   alert(sessionStorage.getItem("userName") + " was disqualified");
+    //   alert(sessionStorage.getItem("userName") + " was disqualified");
 
     //on server:
     //sort board
@@ -195,7 +191,7 @@ function alert_disqualify(acc_magnitude){
   } else {
     //ie: if acc_magnitude<upper_threshold*0.75 && acc_magnitude>lower_threshold
     //make their screen green
-   updateState.innerHTML = "Safe" + acc_magnitude.toFixed(2);
+    updateState.innerHTML = "Safe" + acc_magnitude.toFixed(2);
 
     document.body.style.background = "green";
   }
@@ -216,47 +212,47 @@ function getAccel() {
             yOutput.innerHTML = event.acceleration.y.toFixed(2);
             zOutput.innerHTML = event.acceleration.z.toFixed(2);
             updateState.innerHTML = "Started motion sensing";
-            
 
             iOSAccMagnitude = Math.sqrt(
               event.acceleration.x * event.acceleration.x +
-              event.acceleration.y * event.acceleration.y +
-              event.acceleration.z * event.acceleration.z)
+                event.acceleration.y * event.acceleration.y +
+                event.acceleration.z * event.acceleration.z
+            );
 
-                //process magnitude
-                
-            normOutput.innerHTML = iOSAccMagnitude.toFixed(2)
+            //process magnitude
+
+            normOutput.innerHTML = iOSAccMagnitude.toFixed(2);
             alert_disqualify(iOSAccMagnitude);
           });
         }
       })
       .catch(console.error);
-      alert_disqualify(iOSAccMagnitude);
-  } else  {
+    alert_disqualify(iOSAccMagnitude);
+  } else {
     // alert_disqualify(updateReadings())
     // non iOS 13+
-    updateReadings()
-    console.log('alter_disqualify')
-    lacl = new LinearAccelerationSensor({frequency: 60});
-    lacl.addEventListener('reading', () => {
-      acc_magnitude = Math.sqrt(lacl.x*lacl.x + lacl.y*lacl.y + lacl.z*lacl.z)
+    updateReadings();
+    console.log("alter_disqualify");
+    lacl = new LinearAccelerationSensor({ frequency: 60 });
+    lacl.addEventListener("reading", () => {
+      acc_magnitude = Math.sqrt(
+        lacl.x * lacl.x + lacl.y * lacl.y + lacl.z * lacl.z
+      );
       // alert("Acceleration along the X-axis " + acl.x + ", Y-axis: " + acl.y + ", Z-axis: " + acl.z);
-        alert_disqualify(acc_magnitude);
+      alert_disqualify(acc_magnitude);
     });
     lacl.start();
   }
-  
 }
 
 setInterval(getAccel(), 500);
 
-  DeviceMotionEvent.requestPermission().then((response) => {
-    if (response == "granted") {
-      console.log("accelerometer permission granted");
-      // Do stuff here
-    }
-  });
-
+DeviceMotionEvent.requestPermission().then((response) => {
+  if (response == "granted") {
+    console.log("accelerometer permission granted");
+    // Do stuff here
+  }
+});
 
 function changeToBlue() {
   document.body.style.background = "blue";
@@ -270,3 +266,6 @@ function changeToRed() {
   document.body.style.background = "red";
 }
 
+setInterval(function () {
+  updateMag.innerHTML = acc_magnitude.toFixed(2);
+}, 100);
