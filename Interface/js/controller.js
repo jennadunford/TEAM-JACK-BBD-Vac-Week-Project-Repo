73710d81e77fac2e713bf2794bdbcb94701348ab
tomSteve25 <tds251000@gmail.com>
2022-07-07 08:52:32 +0200

    ?

--- Interface/js/controller.js
+++ Interface/js/controller.js
@@ -116,7 +116,7 @@ socket.on("gameStarted", () => {
   //will start users' accelerometer
   console.log("start game");
   //sessionStorage.setItem("Playing", true);
-  location.href = "https://jack-joust.herokuapp.com/playerScreen.html";
+  window.location = "https://jack-joust.herokuapp.com/playerScreen.html";
   //start accelerometer
 });
 
