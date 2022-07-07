9ab3ff7e24a28f03c51546d396f353dc865b43bc
tomSteve25 <tds251000@gmail.com>
2022-07-07 08:44:05 +0200

    ?

--- Interface/js/controller.js
+++ Interface/js/controller.js
@@ -116,7 +116,7 @@ socket.on("gameStarted", () => {
   //will start users' accelerometer
   console.log("start game");
   //sessionStorage.setItem("Playing", true);
-  window.location.href = "./playerScreen.html";
+  window.location.href = "playerScreen.html";
   //start accelerometer
 });
 
